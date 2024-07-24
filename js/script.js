a698d41989bbef44d29e935d717403a89e654c46
hayyanshaikh <hayyanshaikh55@gmail.com>
2024-07-24 17:48:20 +0500

    Update header and title

--- js/script.js
+++ js/script.js
@@ -2,9 +2,7 @@ $(function() {
   $(window).resize(function() {
     convertToOffcanvas();
   }).trigger('resize');
-
-  counter();
-
+  
   var forms = $('.needs-validation');
   forms.each(function() {
     $(this).on('submit', function(event) {
@@ -25,36 +23,4 @@ const convertToOffcanvas = () => {
     $("#menu").show();
   }, 100);
   $(window).width() < 992 ? menu.addClass('offcanvas offcanvas-end shows') : menu.removeClass('offcanvas offcanvas-start');
-}
-
-const counter = () => {
-  const counters = $('.counter-number');
-  const speed = 400;
-
-  counters.each(function() {
-    const counter = $(this);
-    const target = counter.data('count');
-    let count = 0;
-
-    const updateCount = function() {
-      const increment = target / speed;
-      count += increment;
-
-      if (count < target) {
-        counter.text(Math.ceil(count));
-        setTimeout(updateCount, 1);
-      } else {
-        counter.text(target);
-      }
-    };
-
-    const observer = new IntersectionObserver(function(entries) {
-      if (entries[0].isIntersecting) {
-        updateCount();
-        observer.unobserve(counter[0]);
-      }
-    }, { threshold: 0.5 });
-
-    observer.observe(counter[0]);
-  });
 }
