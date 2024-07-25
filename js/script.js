156fcacd03648405a5a008445b470f857562435f
hayyanshaikh <hayyanshaikh55@gmail.com>
2024-07-25 13:13:42 +0500

    create hero section

--- js/script.js
+++ js/script.js
@@ -13,6 +13,21 @@ $(function() {
       $(this).addClass('was-validated');
     });
   });
+
+  var heroSlider = new Swiper('.hero_slider', {
+      slidesPerView: 1,
+      speed: 1000,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
 });
 
 const convertToOffcanvas = () => {
